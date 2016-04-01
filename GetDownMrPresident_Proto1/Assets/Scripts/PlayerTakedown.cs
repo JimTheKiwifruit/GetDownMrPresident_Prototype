0fa0cbe35aa0cfa5432f2b570723ca475b2d2865
James Penman <James.Pen@live.com>
2016-04-01 19:47:57 +1300

    Added player + takedown + assassins

---
+++ GetDownMrPresident_Proto1/Assets/Scripts/PlayerTakedown.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerTakedown : MonoBehaviour {
+
+	public float minDistance = 1.5f;
+
+	void Update() {
+		if (Input.GetButtonDown("Takedown")) {
+			Takedown();
+		}
+	}
+
+	public void Takedown() {
+		GameObject[] assassins = GameObject.FindGameObjectsWithTag("Assassin");
+		foreach (GameObject ass in assassins) {
+			if (Vector3.Distance(ass.transform.position, transform.position) < minDistance) {
+				//Destroy(ass);
+				ass.GetComponent<Collider>().isTrigger = false;
+				ass.AddComponent<Rigidbody>().mass = 0.25f;
+				ass.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, 80));
+				Destroy(ass.GetComponent<NavMeshAgent>());
+				Destroy(ass.GetComponent<NPC>());
+				gameObject.tag = "Untagged";
+				GetComponent<Rigidbody>().AddForce(Vector3.Normalize(ass.transform.position - transform.position) * 50, ForceMode.VelocityChange);
+			}
+		}
+	}
+	
+}
