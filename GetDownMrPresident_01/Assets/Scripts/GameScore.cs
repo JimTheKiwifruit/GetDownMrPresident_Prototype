527feda4d0fcd35bde96cf7f0e925bc310675ded
James Penman <James.Pen@live.com>
2016-05-27 09:49:18 +1200

    Added ting

---
+++ GetDownMrPresident_01/Assets/Scripts/GameScore.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+    
+
+    int duration;
+    int seconds;
+    int mins;
+    string durationString;
+
+
+    // Use this for initialization
+    void Start () {
+        updateDuration();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateDuration();
+
+        print(durationString);
+    }
+
+    void updateDuration()
+    {
+        duration = (int)Time.time;
+        seconds = duration % 60;
+        mins = (duration - mins) / 60;
+        durationString = mins.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
