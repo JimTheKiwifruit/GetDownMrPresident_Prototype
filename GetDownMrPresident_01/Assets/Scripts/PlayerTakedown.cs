7b4da67fe4a917bd5e67a42348b46d4529ed2178
Guest User <Guest@c-05-06-lab.mddn.vuw.ac.nz>
2016-06-10 11:08:21 +1200

    Added updated takedown Script

--- GetDownMrPresident_01/Assets/Scripts/PlayerTakedown.cs
+++ GetDownMrPresident_01/Assets/Scripts/PlayerTakedown.cs
@@ -23,7 +23,48 @@ public class PlayerTakedown : MonoBehaviour {
 			Takedown();
 		}
 	}
+	
+	public bool TargetInRange(PlayerTakedown target){
+		return Vector3.Distance(target.transform.position, this.transform.position) < minDistance;
+	}
+
+	public void DoTakedown(PlayerTakedown target){
+		target.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		target.gameObject.GetComponent<Rigidbody>().useGravity = true;
+		target.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+		target.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, 80));
+		//Destroy(target.gameObject.GetComponent<PlayerTakedown>());
+		//Destroy(target.gameObject.GetComponent<PlayerMovement>());
+		//Destroy(target.gameObject.GetComponent<NPCPresidentAI>());
+		//Destroy(target.gameObject.GetComponent<NavMeshAgent>());
+		//gameObject.tag = "Untagged";
+		GetComponent<Rigidbody>().AddForce(Vector3.Normalize(target.transform.position - transform.position) * 35, ForceMode.VelocityChange);
+	}
+
+	
 	public void Takedown() {
+		string name = this.name;
+		PlayerTakedown[] targets = FindObjectsOfType<PlayerTakedown>();
+		foreach (PlayerTakedown target in targets) {
+			if (target.playerNum == this.playerNum)
+				continue;
+			if( name.Equals("Dev_Player_01 (Assassin)") && target.name.Equals("President") && TargetInRange(target)) {
+				RoundManager.main.PresidentDown();
+				DoTakedown(target);
+			} else if ( name.Equals("Dev_Player_01 (Bodyguard)") && target.name.Equals("Dev_Player_01 (Assassin)") && TargetInRange(target)) {
+				RoundManager.main.PresidentSaved();
+				DoTakedown(target);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		/**
 		PlayerTakedown[] targets = FindObjectsOfType<PlayerTakedown>();
 		foreach (PlayerTakedown target in targets) {
 			if (target.playerNum == this.playerNum)
@@ -32,35 +73,24 @@ public class PlayerTakedown : MonoBehaviour {
 				print("takedonw!!!! " + target.playerNum + "---" + this.playerNum);
 				print(Vector3.Distance(target.transform.position, transform.position));
 
-				if (playerNum == 1) {
+				if (playerNum == 1 && this.name.Equals("Dev_Player_01 (Assassin)") {
 					if (target.gameObject.CompareTag("President")) {
 						RoundManager.main.PresidentDown();
 					} else {
 						break;
 					}
-				} else if (playerNum == 2) {
-					if (target.gameObject.CompareTag("President")) {
-						continue;
-					} else {
+				} else if (playerNum == 2 && this.name.Equals("Dev_Player_01 (Bodyguard)") {
+					if (target.gameObject.CompareTag("Assassin")) {
 						RoundManager.main.PresidentSaved();
 					}
 				} else {
 					continue;
 				}
 
-				target.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-				target.gameObject.GetComponent<Rigidbody>().useGravity = true;
-				target.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-				target.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, 80));
-				Destroy(target.gameObject.GetComponent<PlayerTakedown>());
-				Destroy(target.gameObject.GetComponent<PlayerMovement>());
-				Destroy(target.gameObject.GetComponent<NPCPresidentAI>());
-				Destroy(target.gameObject.GetComponent<NavMeshAgent>());
-				gameObject.tag = "Untagged";
-				GetComponent<Rigidbody>().AddForce(Vector3.Normalize(target.transform.position - transform.position) * 35, ForceMode.VelocityChange);
-				break;
+				
 			}
 		}
+		**/
 	}
 
 }
